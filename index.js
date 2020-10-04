d9fec1f34b1cb43827acba486d04534f6e56068c
unibreakfast <unibreakfast1@gmail.com>
2020-10-04 15:00:26 +0300

    validate function works

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+import 'c4console'
 import validate from './validate.js'
+
+
+const inputValues1 = {
+  login: ' ',
+  password: ' ',
+  email: ''
+}
+
+const requirements1 = [
+  {
+    field: 'password', is: /.{4}/,
+    issue: 'has to be at least 4 characters long'
+  },
+  {
+    field: 'password', not: /\s/,
+    issue: 'should not include spaces'
+  },
+
+  {
+    field: 'login', hasNo: /[^\w]/,
+    issue: 'only alpanumeric characters allowed'
+  }
+]
+
+
+validate(inputValues1, requirements1).c()
+
+
+setTimeout(() => { }, 7e7)
--- validate.js
+++ validate.js
@@ -1,3 +1,11 @@
-export default function validate(fields, checks) {
-
+export default function validate(values, requirements) {
+  return requirements.filter(req => {
+    if (req.is || req.has) {
+      const rule = req.is || rule.has
+      return !rule.test(values[req.field])
+    } else if (req.not || req.hasNo || req.isNot) {
+      const rule = req.not || req.hasNo || req.isNot
+      return rule.test(values[req.field])
+    }
+  }).map(({field, issue}) => ({field, issue}))
 }
